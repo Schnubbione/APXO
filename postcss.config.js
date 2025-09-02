da38f5bede40c1f7edc6c139b39fc6727ef37c3e
Schnubbione <australien@gmx.de>
2025-09-02 21:39:44 +0200

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+export default config;
+
+```parameter>
+<parameter name="oldString">module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+```
