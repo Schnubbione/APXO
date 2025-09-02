d748f243c2bda54388f2a04b7c0259c0f4230eb7
Schnubbione <australien@gmx.de>
2025-09-02 21:45:20 +0200

    Clean PostCSS config (CommonJS plugins object)

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,6 @@
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-};
-
-```parameter>
-<parameter name="oldString">module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
-
-```
+};
