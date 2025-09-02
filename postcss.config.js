b7fb00ea2135f29cd27bcfba7881a6937d3a97e4
Schnubbione <australien@gmx.de>
2025-09-02 21:38:46 +0200

    Fix PostCSS config for ES modules

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
-export default {
+const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+
+export default config;
 
 ```parameter>
 <parameter name="oldString">module.exports = {
